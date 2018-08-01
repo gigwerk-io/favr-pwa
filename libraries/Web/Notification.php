104b346b0d948bbc079ab9082542a401db3826dd
solomonantoine <solomon.antoine@guns.com>
2018-08-01 15:38:39 -0500

    Update to Twilio

---
+++ libraries/Web/Notification.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomonantoine
+ * Date: 8/1/18
+ * Time: 1:16 PM
+ */
+
+class Web_Text
+{
+    public function sendNotification()
+    {
+        return $this;
+    }
+
+
+}
