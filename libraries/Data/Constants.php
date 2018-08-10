f23f5d2fd93c7486923c21a05217ef19b72d597e
Haron Arama <arama006@umn.edu>
2018-08-09 21:11:26 -0500

    new constant for images

--- libraries/Data/Constants.php
+++ libraries/Data/Constants.php
@@ -26,7 +26,8 @@ class Data_Constants
     const MAXIMUM_IMAGE_UPLOAD_COUNT = 3;
     const MAXIMUM_IMAGE_UPLOAD_SIZE = 5242880; // 5 Mb
     const IMAGE_UPLOAD_FILE_PATH = "/Applications/XAMPP/xamppfiles/favr-request-images/";
-
+    const IMAGE_UPLOAD_PROFILE_IMAGE_FILE_PATH = "/Applications/XAMPP/xamppfiles/favr-profile-images/";
+    
     // database marketplace task status constants
     const DB_TASK_STATUS_REQUESTED = "Requested";
     const DB_TASK_STATUS_PENDING_APPROVAL = "Pending Approval";
