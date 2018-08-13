0430a2b8805d9766d4c32154d87180dbe4219b1a
Haron Arama <arama006@umn.edu>
2018-08-12 22:55:14 -0500

    constants

--- libraries/Data/Constants.php
+++ libraries/Data/Constants.php
@@ -13,14 +13,14 @@ class Data_Constants
 {
     // Product version
     const PRODUCT_VERSION = "0.1.1";
-    
+
     // database connection constants
-    const DB_DSN = "mysql:dbname=local_favr;host=localhost";
-    const DB_USERNAME = "haron";
-    const DB_PASSWORD = "Ha7780703";
-    
+    const DB_DSN = "mysql:dbname=local_favr;host=favr.cgfeyejwt7qv.us-east-2.rds.amazonaws.com";
+    const DB_USERNAME = "Solomon04";
+    const DB_PASSWORD = "Nomolos.99";
+
     // Root path
-    const ROOT_PATH = "http://localhost/favr-pwa";
+    const ROOT_PATH = "https://askfavr.com/favr-pwa";
 
     // image upload constants
     const MAXIMUM_IMAGE_UPLOAD_COUNT = 3;
