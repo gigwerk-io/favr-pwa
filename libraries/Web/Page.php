f564cc0d2235d760a73677142c00f8a49b723a57
Haron Arama <arama006@umn.edu>
2018-08-13 11:30:45 -0500

    Updates to edit profile and implementation of user classes

--- libraries/Web/Page.php
+++ libraries/Web/Page.php
@@ -445,7 +445,7 @@ class Web_Page
             <meta charset="utf-8">
             <!--            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
             <meta name="viewport"
-                  content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, shrink-to-fit=yes, user-scalable=yes"/>
+                  content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, shrink-to-fit=yes, user-scalable=no"/>
             <meta name="HandheldFriendly" content="true"/>
             <meta name="description"
                   content="Post job requests at your price and have access to verified freelancers for open tasks. Chat with friends and trade FAVRs.">
@@ -462,7 +462,7 @@ class Web_Page
             <link rel="manifest" href="<?php echo $this->root_path; ?>/manifest.json">
 
             <meta name="apple-mobile-web-app-capable" content="yes">
-            <meta name="apple-mobile-web-app-status-bar-style" content="default">
+            <meta name="apple-mobile-web-app-status-bar-style" content="black">
             <meta name="apple-mobile-web-app-title" content="FAVR">
 
             <!-- iOS -->
@@ -580,7 +580,7 @@ class Web_Page
                     $task_price = $row['task_price'];
                     $task_status = $row['task_status'];
                     $task_rating = $row['task_rating'];
-                    $review = $row['task_optional_service_review'];
+                    $review = stripslashes($row['task_optional_service_review']);
 
                     $freelancerInfo = $this->getUserInfo($freelancer_id);
                     $customerInfo = $this->getUserInfo($customer_id);
@@ -947,8 +947,8 @@ class Web_Page
                                     </button>";
 
                         // profile edit modal
-                        echo "<div class=\"modal fade\" id=\"profileEditModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"profileEditTitle\" aria-hidden=\"true\">
-                                  <form action='$this->root_path/components/profile/?navbar=active_profile' method='post' enctype='multipart/form-data'>
+                        echo "    <div class=\"modal fade\" id=\"profileEditModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"profileEditTitle\" aria-hidden=\"true\">
+                                    <form action='$this->root_path/components/profile/?navbar=active_profile' method='post' enctype='multipart/form-data'>
                                       <div class=\"modal-dialog\" role=\"document\">
                                         <div class=\"modal-content\">
                                           <div class=\"modal-header\">
@@ -972,8 +972,15 @@ class Web_Page
                                   </form>
                                 </div>";
                         ?>
-<!--                        <img data-src="holder.js/7remx7rem?theme=thumb&amp;bg=007bff&amp;fg=007bff&amp;size=1" alt="128x128" class="rounded" style="bottom: 3.5rem;width: 7rem;height: 7rem;position: relative;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22128%22%20height%3D%22128%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20128%20128%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164a9f2d749%20text%20%7B%20fill%3A%23007bff%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A6pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164a9f2d749%22%3E%3Crect%20width%3D%22128%22%20height%3D%22128%22%20fill%3D%22%23007bff%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2248.4296875%22%20y%3D%2266.7%22%3E128x128%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">-->
-                        <h3><?php echo $userInfo['first_name'] . " " . $userInfo['last_name']; ?><i class="material-icons text-primary">verified_user</i></h3>
+                        <h3>
+                            <?php
+                                echo $userInfo['first_name'] . " " . $userInfo['last_name'];
+
+                                if ($userInfo['class'] == Data_Constants::DB_USER_CLASS_VERIFIED) {
+                                    echo "<i class=\"material-icons text-primary\">verified_user</i>";
+                                }
+                            ?>
+                        </h3>
                     <?php
                         $ratings = unserialize($userInfo['rating']);
                         $this->renderFavrProfileRating($ratings);
@@ -2771,6 +2778,7 @@ class Web_Page
         if (isset($requestID, $customerID, $freelancerID, $requestRating, $timestamp)) {
             // complete request
             $complete = Data_Constants::DB_TASK_STATUS_COMPLETED;
+            $requestReview = addslashes($requestReview);
             $update_request_query = "UPDATE marketplace_favr_requests 
                                      SET task_status = '$complete',
                                          task_rating = '$requestRating',
