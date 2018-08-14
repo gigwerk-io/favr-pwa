a579c3818d16ef13581159bf02308d21c9c0043f
solomonantoine <solomon.antoine@guns.com>
2018-08-14 00:13:17 -0500

    New Test Chat Page -- Testing Purposes

---
+++ home/chat/test-chat.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haronarama
+ * Date: 7/13/18
+ * Time: 11:37 AM
+ */
+session_start();
+include($_SERVER['DOCUMENT_ROOT'] . "/favr-pwa/include/autoload.php");
+
+// component constants
+$PAGE_ID = 5;
+$USER = "";
+$list = new Web_Chat();
+
+if (isset($_SESSION['user_info'])) {
+    $USER = $_SESSION['user_info']['username']; // user is set from initial configuration
+}
+
+$page = new Web_Page($PAGE_ID, $USER);
+
+//header("Location: ../../assets/css/chat.css");
+
+$page->setTitle("Chat");
+$page->addStylesheet("<link rel='stylesheet' href='$page->root_path/assets/css/chat.css'>");
+$page->renderHeader();
+if (isset($_POST['Text']))
+{
+    $message = array(
+        "File" => $_GET['file'],
+        "To" => $_GET['to'],
+        "Text" => $_POST['Text'],
+        "Time" => time()
+    );
+    $list->updateMessage($message);
+}
+?>
+<!--    <meta http-equiv="refresh" content="30"/>-->
+
+    <div class="messaging m-0 p-0">
+        <div class="inbox_msg">
+            <div class="inbox_people">
+                <div class="headind_srch">
+                    <div class="recent_heading">
+                        <h4>Recent</h4>
+                    </div>
+                    <div class="srch_bar">
+                        <div class="stylish-input-group">
+                            <input type="text" class="search-bar" placeholder="Search">
+                            <span class="input-group-addon">
+                <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                </span></div>
+                    </div>
+                </div>
+                <?php
+                $list->listChat();
+                echo "<div id=\"status\">
+                </div>";
+                ?>
+            </div>
+            <div class="web-messaging-contact mesgs p-0">
+                <div class="msg_history p-2 pt-5">
+                    <?php
+                    if(isset($_GET['file']))
+                    {
+                        $list->displayMessage($_GET['file']);
+                    }
+                    ?>
+                </div>
+                <div class="type_msg m-0">
+                    <div class="input_msg_write">
+                        <form action="<?php echo "test-chat.php?file=" . $_GET['file'] . "&to=" . $_GET['to']; ?>" method="post" id="Message">
+                        <textarea onkeyup="resetTimer = true" style="border-radius: 0;" type="text" class="form-control m-0 pt-2" name="Text" placeholder="Type a message..."></textarea>
+                        </form>
+                        <button class="msg_send_btn text-center p-1" form="Message" type="submit"><i class="material-icons"
+                                                                                      aria-hidden="true">send</i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script>
+    function refreshPageUnlessFocusedOn (el) {
+
+        setInterval(function () {
+            if(el !== document.activeElement) {
+                document.location.reload();
+            }
+        }, 10000)
+
+    }
+
+    refreshPageUnlessFocusedOn(document.querySelector('textarea'));
+</script>
+<?php
+$page->addScript("
+<script>
+//    window.location.hash = '#recent-15';
+    if (screen.width <= 767.98) {
+        window.location.hash = '#recent-15';
+    }
+</script>");
+$page->renderFooter(false);
+?>
