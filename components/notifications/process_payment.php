c021bf527d73d263393063527f77e1732261bf70
Solomon04 <Antoinesolomon5@gmail.com>
2018-08-16 23:19:05 -0500

    Update to process payment

--- components/notifications/process_payment.php
+++ components/notifications/process_payment.php
@@ -5,6 +5,8 @@
  * Date: 8/16/18
  * Time: 2:58 PM
  */
+
+include($_SERVER['DOCUMENT_ROOT'] . "/favr-pwa/include/autoload.php");
 require_once '../../libraries/Api/Stripe/init.php';
 $payment = new Web_Payment();
 if(isset($_POST['stripeToken'])){
