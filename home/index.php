bf187102ea74d52340ee2d2264b7e9d2ceeb49d0
solomonantoine <solomon.antoine@guns.com>
2018-08-20 15:07:56 -0500

    Analytics Tools Added

--- home/index.php
+++ home/index.php
@@ -80,6 +80,35 @@ $page->renderFavrMarketplace($_SESSION['scope'], $_SESSION['filter_marketplace_b
 
 $page->addScript("
     <script>
+    <!-- Hotjar Tracking Code for askfavr.com -->
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:893054,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+
+    
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1650241185015256');
+        fbq('track', 'PageView')
+    
+        <noscript>
+            <img height=\"1\" width=\"1\" style=\"display:none\"
+            src=\"https://www.facebook.com/tr?id=1650241185015256&ev=PageView&noscript=1\"/>
+        </noscript>
+        
+
+<!-- End Facebook Pixel Code -->
+
     $(function(){
         var dtToday = new Date();
         
