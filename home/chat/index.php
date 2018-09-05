2227216c119bf5291ce9ec33c3f8d1349f55ba45
solomonantoine <solomon.antoine@guns.com>
2018-09-05 14:42:21 -0500

    New Chat Feature, Updated

--- home/chat/index.php
+++ home/chat/index.php
@@ -11,7 +11,7 @@ include($_SERVER['DOCUMENT_ROOT'] . "/favr-pwa/include/autoload.php");
 // component constants
 $PAGE_ID = 5;
 $USER = "";
-$list = new Web_Chat();
+$chat = new Web_Chat();
 
 if (isset($_SESSION['user_info'])) {
     $USER = $_SESSION['user_info']['username']; // user is set from initial configuration
@@ -24,15 +24,9 @@ $page = new Web_Page($PAGE_ID, $USER);
 $page->setTitle("Chat");
 $page->addStylesheet("<link rel='stylesheet' href='$page->root_path/assets/css/chat.css'>");
 $page->renderHeader();
-if (!empty($_POST['Text']))
-{
-    $message = array(
-        "File" => $_GET['file'],
-        "To" => $_GET['to'],
-        "Text" => $_POST['Text'],
-        "Time" => time()
-    );
-    $list->updateMessage($message);
+
+if(isset($_POST['message'])){
+    $chat->sendMessage($_GET['chat_room'], $_SESSION['user_info']['id'], $_POST['message']);
 }
 ?>
     <!--    <meta http-equiv="refresh" content="30"/>-->
@@ -54,84 +48,67 @@ if (!empty($_POST['Text']))
                 </div>
                 <?php
                 echo "<div class=\"inbox_chat\">";
-                $list->listChat();
+                $chat->getChatList();
                 echo "</div>";
                 echo "<div id=\"status\">
                 </div>";
                 ?>
             </div>
             <div class="web-messaging-contact mesgs p-0">
-                <div class="msg_history p-2 pt-5">
+                <div class="msg_history p-2 pt-5" onload="ajax();">
                     <?php
-                    if(isset($_GET['file']))
+                    if(isset($_GET['chat_room']))
                     {
-                        $list->displayMessage($_GET['file']);
+                        $chat_id = $_GET['chat_room'];
+                        echo "<div id=\"chat\"></div>";
+                        //$chat->getAllMessages($_GET['chat_room']);
                     }
                     ?>
                 </div>
-                <div class="type_msg m-0">
-                    <div class="input_msg_write">
-                        <form action="<?php echo "?file=" . $_GET['file'] . "&to=" . $_GET['to']; ?>" method="post" id="Message">
-                            <textarea onkeyup="resetTimer = true" style="border-radius: 0;" type="text" class="form-control m-0 pt-2" name="Text" placeholder="Type a message..."></textarea>
-                        </form>
-                        <button class="msg_send_btn text-center p-1" form="Message" type="submit"><i class="material-icons"
-                                                                                                     aria-hidden="true">send</i></button>
-                    </div>
-                </div>
+                <?php
+                if(isset($_GET['chat_room'])){
+                    echo " <div class=\"type_msg m-0\">
+                        <div class=\"input_msg_write\">
+                                <form action=\"index.php?chat_room=$chat_id\" method=\"post\" id=\"Message\">
+                                    <textarea style=\"border-radius: 0;\" type=\"text\" class=\"form-control m-0 pt-2\" name=\"message\" placeholder=\"Type a message...\"></textarea>
+                                </form>
+                                <button class=\"msg_send_btn text-center p-1\" form=\"Message\" type=\"submit\"><i class=\"material-icons\"
+                                                                                                             aria-hidden=\"true\">send</i></button>
+                            </div>
+                        </div></div>";
+                }
+                ?>
+<!--                <div class="type_msg m-0">-->
+<!--                    <div class="input_msg_write">-->
+<!--                        <form action="index.php" method="post" id="Message">-->
+<!--                            <textarea onkeyup="resetTimer = true" style="border-radius: 0;" type="text" class="form-control m-0 pt-2" name="message" placeholder="Type a message..."></textarea>-->
+<!--                        </form>-->
+<!--                        <button class="msg_send_btn text-center p-1" form="Message" type="submit"><i class="material-icons"-->
+<!--                                                                                                     aria-hidden="true">send</i></button>-->
+<!--                    </div>-->
+<!--                </div>-->
             </div>
         </div>
     </div>
-    <script>
-        // function refreshPageUnlessFocusedOn (el) {
-        //
-        //     setInterval(function () {
-        //         if(el !== document.activeElement) {
-        //             document.location.reload();
-        //         }
-        //     }, 12500)
-        //
-        // }
-        //
-        // refreshPageUnlessFocusedOn(document.querySelector('textarea'));
-    </script>
 <?php
+
 $page->addScript("
-<script>
-//    window.location.hash = '#recent-15';
-    if (screen.width <= 767.98) {
-        window.location.hash = '#recent-15';
-    }
-</script>
-<!-- Hotjar Tracking Code for askfavr.com -->
     <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:893054,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        function ajax() {
+            var req = new XMLHttpRequest();
+
+            req.onreadystatechange = function () {
+                if(req.readyState == 4 && req.status==200){
+                    document.getElementById('chat').innerHTML = req.responseText;
+                }
+            }
+            req.open('GET', 'process.php?chat_room=$chat_id',true);
+            req.send();
+        }
+        setInterval(function () {
+            ajax();
+        }, 1000)
     </script>
-   
-    <!-- Facebook Pixel Code -->
-        <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1650241185015256');
-        fbq('track', 'PageView');
-        </script>
-        <noscript>
-            <img height=\"1\" width=\"1\" style=\"display:none\"
-            src=\"https://www.facebook.com/tr?id=1650241185015256&ev=PageView&noscript=1\"/>
-        </noscript>
-        <!-- End Facebook Pixel Code -->
-    <script>");
+");
 $page->renderFooter(false);
 ?>
