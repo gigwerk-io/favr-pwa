3e3038a2cc5773b8f5e0deaa5e7fc3ff461c080e
Solomon04 <Antoinesolomon5@gmail.com>
2018-09-07 08:05:52 -0500

    Update to Notifications

--- components/notifications/firebase/firebase-messaging-sw.js
+++ components/notifications/firebase/firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/5.0.0/firebase-app.js');
-importScripts('/__/firebase/5.0.0/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-messaging.js');
+//importScripts('/__/firebase/init.js');
 
 var config = {
   apiKey: "AIzaSyChPH0J3U7mrS_fYxKJehQJBQpBhlCFob8",
--- components/notifications/firebase/notification.php
+++ components/notifications/firebase/notification.php
@@ -2,8 +2,8 @@
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, "https://fcm.googleapis.com/fcm/send");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n  \"notification\": {\n    \"title\": \"FAVR\",\n    \"body\": \"A new request has been posted in the marketplace. Be the first to complete!\",\n    \"icon\": \"https://askfavr.com/favr-pwa/assets/brand/favr_logo_rd.png\",\n    \"click_action\": \"http://localhost:8081\"\n  },\n  \"to\": \"fzjv8ICNSYE:APA91bEmm1oAUGghiqk_vdF1GDX9yEXl3T2NPbNHyK0iT7T16y27DgRTTU-ypEj0FostsAFhaXQvQloybxHQK6NeuKIKHNg5i9-rSDNqt6Skc4xyLqIYFToxsqxR2aRdYhcHbnPMMDA6\"\n}");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, "https://askfavr.com/favr-pwa");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n  \"notification\": {\n    \"title\": \"FAVR\",\n    \"body\": \"A new request has been posted in the marketplace. Be the first to complete!\",\n    \"icon\": \"https://askfavr.com/favr-pwa/assets/brand/favr_logo_rd.png\",\n    \"click_action\": \"http://localhost:1234/favr-pwa/components/notifications/firebase/1\"\n  },\n  \"to\": \"fzjv8ICNSYE:APA91bEmm1oAUGghiqk_vdF1GDX9yEXl3T2NPbNHyK0iT7T16y27DgRTTU-ypEj0FostsAFhaXQvQloybxHQK6NeuKIKHNg5i9-rSDNqt6Skc4xyLqIYFToxsqxR2aRdYhcHbnPMMDA6\"\n}");
 curl_setopt($ch, CURLOPT_POST, 1);
 
 $headers = array();
@@ -16,3 +16,13 @@ if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
 curl_close ($ch);
+
+//curl -X POST -H "Authorization: key=AAAAXaruOsk:APA91bEsGAqHjZBsOdP-StRfYfsc4tq4-zym7HPveLzATIO2uzZ0TC6Z3K2WL_-YMQ3lFd6I9J81fPz9cZ4JKs9EkZJE1hZBoLtFIPruoPKc-28n5O9ZO9JBPuj9HFpvcot07OWvT9v3" -H "Content-Type: application/json" -d '{
+//  "notification": {
+//    "title": "FAVR",
+//    "body": "A new request has been posted in the marketplace. Be the first to complete!",
+//    "icon": "https://askfavr.com/favr-pwa/assets/brand/favr_logo_rd.png",
+//    "click_action": "https://askfavr.com/favr-pwa"
+//  },
+//  "to": "fzjv8ICNSYE:APA91bEmm1oAUGghiqk_vdF1GDX9yEXl3T2NPbNHyK0iT7T16y27DgRTTU-ypEj0FostsAFhaXQvQloybxHQK6NeuKIKHNg5i9-rSDNqt6Skc4xyLqIYFToxsqxR2aRdYhcHbnPMMDA6"
+//}' "https://fcm.googleapis.com/fcm/send"
