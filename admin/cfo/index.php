3ceeaa2dcf0485ef22e3b88bd5888e73cab77bd5
solomonantoine <solomon.antoine@guns.com>
2018-09-18 21:57:43 -0500

    Created A CFO Admin Page for Ken to access critical data for financial reporting.

---
+++ admin/cfo/index.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomonantoine
+ * Date: 9/14/18
+ * Time: 10:16 AM
+ */
+session_start();
+include($_SERVER['DOCUMENT_ROOT'] . "/favr-pwa/include/autoload.php");
+
+// component constants
+$USER = "";
+$ALERT_MESSAGE = "";
+
+if (isset($_SESSION['user_info'])) {
+    $USER = $_SESSION['user_info']['username']; // user is set from initial configuration
+}
+
+
+
+$page = new Web_Page($USER);
+$page->setTitle("CFO Admin");
+$page->renderAdminHeader($_SESSION['user_info']['id']);
+if(isset($_POST['payment'])){
+    $financial = new Web_Financials();
+    $financial->fileHandler($_FILES['csv']);
+}
+
+if(isset($_POST['transfer'])){
+    $financial = new Web_Financials();
+    $financial->transferFileHandler($_FILES['csv']);
+}
+
+
+echo "
+<body>
+<div class=\"container-fluid\">
+    <div class=\"row\">";
+        $page->renderAdmin("CFO");
+        $page->cfoDashboard();
+    echo "</div>
+</div>";
+
+
+
+$page->addScript("
+<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>
+<script>window.jQuery || document.write('<script src=\"https://getbootstrap.com/docs/4.0/assets/js/vendor/jquery-slim.min.js\"><\/script>')</script>
+<script src=\"https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js\"></script>
+<script src=\"https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js\"></script>
+
+<!-- Icons -->
+<script src=\"https://unpkg.com/feather-icons/dist/feather.min.js\"></script>
+<script>
+    feather.replace()
+</script>
+
+<!-- Graphs -->
+<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js\"></script>
+<script>
+    var ctx = document.getElementById(\"myChart\");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [\"Sunday\", \"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"],
+            datasets: [{
+                data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+                lineTension: 0,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            legend: {
+                display: false,
+            }
+        }
+    });
+</script>");
+?>
