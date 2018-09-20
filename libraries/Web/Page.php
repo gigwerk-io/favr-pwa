a889cbcc5afa236cc11b66586bbf3c3f1226e657
solomonantoine <solomon.antoine@guns.com>
2018-09-20 10:38:15 -0500

    Added Hotjar to Page

--- libraries/Web/Page.php
+++ libraries/Web/Page.php
@@ -641,11 +641,11 @@ class Web_Page
             }
             </script>
 
-            <!-- Hotjar Tracking Code for askfavr.com -->
+            <!-- Hotjar Tracking Code for https://askfavr.com/favr-pwa -->
             <script>
                 (function(h,o,t,j,a,r){
                     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:893054,hjsv:6};
+                    h._hjSettings={hjid:1022094,hjsv:6};
                     a=o.getElementsByTagName('head')[0];
                     r=o.createElement('script');r.async=1;
                     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
